3c7062720ae42e26c7efc420bea2da1315d5bed4
Brandon Phillips <bmp0015@mix.wvu.edu>
2018-03-11 09:21:31 -0400

    Commenting flag creation

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ var assetsPath string
 func processFlags() *daemon.Config {
     cfg := &daemon.Config{}
 
+	// flag (pointer, name (--name), default value, usage)
     flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
     flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=/var/run/postgresql dbname=gowebapp sslmode=disable", "DB Connect String")
     flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
